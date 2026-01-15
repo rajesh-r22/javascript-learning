5b4b0e2b971718ce690cc74e7e65efee5fe86f2f
rajesh-r22 <itsmerajeshreddy01@gmail.com>
2026-01-15 23:52:00 +0530

    Closure with example

---
+++ basics/intro.js
@@ -0,0 +1,40 @@
+//JAVASCRIPT - javascript is a programming language used to make websites interactive(logic,behavior, data handing)
+
+// VARIABLES - variable is a container to store data.
+// ex:- var , let , const
+// var : var is  FUNCTION-SCOPE, and unsafe to use, causes bugs,reassigning /re-declaring property is dangerous .
+// var x = 10;
+// var x = 20; // allowed (problem)
+
+// let :let is safe to use , it is BLOCK-SCOPE, can be reassign , but not re-declare
+// let num= 10;
+// console.log(num);
+// num=30;
+// console.log(num);
+// let num=10 //gives compile time error
+
+// const :const is also safe to use , BLOCK-SCOPED, but cannot reassign value
+
+// closure: a function that remember TouchEvent, variables of outer function, even after outer function is over OR from its lexical environment
+// function outer(){
+//     let count=0;
+//     return function inner(){
+//         count++;
+//         return count;
+//     }
+// }
+
+// const counter=outer();
+// console.log(counter());
+// console.log(counter());
+// console.log(counter());
+
+
+
+
+
+ 
+ 
+
+
+
